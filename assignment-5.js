88c4a9c7436f586d77e10fe31f50a469fbf9f2ff
AshutoshSadhwani <157961012+AshutoshSadhwani@users.noreply.github.com>
2024-03-31 22:31:26 +0530

    Create assignment-5.js

---
+++ assignment-5.js
@@ -0,0 +1,24 @@
+// script.js
+
+// TODO: STEP 0 - Ensure access to randomCategoryShortName variable
+var randomCategoryShortName = ""; // This should be populated with a random category short name
+
+// TODO: STEP 1 - Generate a random category short name
+function generateRandomCategoryShortName() {
+    // Logic to generate a random category short name
+    // Assign the generated value to randomCategoryShortName
+}
+
+// TODO: STEP 2 - Define function to load menu items for a given category
+function loadMenuItems(categoryShortName) {
+    // Logic to load menu items for the given category
+}
+
+// TODO: STEP 3 - Define function to handle click on Specials tile
+function handleSpecialsTileClick() {
+    // Logic to handle click on Specials tile
+    // Use randomCategoryShortName to load menu items for a random category
+}
+
+// TODO: STEP 4 - Attach event listener to Specials tile
+document.getElementById("specials-tile").addEventListener("click", handleSpecialsTileClick);
